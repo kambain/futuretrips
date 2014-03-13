2c56b2a6dc80244291b64d4c1262a676add94a2a
Kameron Bain <kam@craaave.com>
2014-03-13 18:53:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Futuretrips::Application.config.secret_key_base = '9f253286b5d0c19b14c79ceb3d8c16cb7fd7954efe0d8f5314e9332ea4185ee843f2f749108957c5d188dee1a091fbfd9fb186a87e2d11ed38b5d60ae54dac39'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Futuretrips::Application.config.session_store :cookie_store, key: '_futuretrips_session'
